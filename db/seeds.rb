df1c5d038712cc49b75582ca454c4be3b451fdd9
Cataline <catalinebaert@yahoo.com>
2021-09-15 17:24:11 +0200

    Create first endpoint INDEX to get all instances of Task

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Task.destroy_all
+
+puts 'Create 3 initial tasks...'
+Task.create!(name: 'Do yoga', completed: true)
+Task.create!(name: 'Meditate', completed: true)
+Task.create!(name: 'Build first project')
+
+puts 'Finished'
