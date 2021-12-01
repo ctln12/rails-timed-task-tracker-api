b968a0ff3e31c17eadb3760857e1b548aec2949b
Cataline <catalinebaert@yahoo.com>
2021-12-01 14:32:45 +0100

    Add update route for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :tasks, only: [:index, :create, :update, :destroy]
+      resources :sessions, only: :update
     end
   end
 end
