160fdc42f69433d5fa49f6f5f317d550dfc50cb0
Cataline <catalinebaert@yahoo.com>
2021-12-01 17:07:31 +0100

    Refactor sessions controller with best practices

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,16 +1,32 @@
-class Api::V1::SessionsController < ApplicationController
-  def update
-    @session = Session.find(params[:id])
-    if @session.update(session_params)
-      render json: @session
-    else
-      render_error
+# frozen_string_literal: true
+
+module Api
+  module V1
+    # Controller to handle the requests made on the sessions table and that produces json
+    class SessionsController < ApplicationController
+      def update
+        @session = Session.find(params[:id])
+        if @session.update(session_params)
+          render json: @session
+        else
+          render_error
+        end
+      end
+
+      private
+
+      def session_params
+        params.require(:session).permit(
+          :completed,
+          :focus_length, :short_break_length, :long_break_length, :long_break_after,
+          :task_id
+        )
+      end
+
+      def render_error
+        render  json: { errors: @session.errors.full_messages },
+                status: :unprocessable_entity
+      end
     end
-  end
-
-  private
-
-  def session_params
-    params.require(:session).permit(:completed, :focus_length, :short_break_length, :long_break_length, :long_break_after, :task_id)
   end
 end
