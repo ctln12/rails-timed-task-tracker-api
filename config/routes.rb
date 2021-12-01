c4e2560080f4e9847030f27ccf6d679e056740a2
Cataline <catalinebaert@yahoo.com>
2021-12-01 17:15:52 +0100

    Set root path to api/v1/tasks#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'api/v1/tasks#index'
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :tasks, only: [:index, :create, :update, :destroy]
+      resources :tasks, only: %i[index create update destroy]
       resources :sessions, only: :update
     end
   end
