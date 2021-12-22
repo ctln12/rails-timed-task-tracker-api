afcd01d03f853cc134250e02084a65838d4d01dd
Cataline Baert <snuf_kin@yahoo.com>
2021-12-22 16:07:01 +0100

    Add a default session to each task newly created in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,13 @@ Task.destroy_all
 Session.destroy_all
 
 puts 'Create 3 initial tasks...'
-Task.create!(name: 'Do yoga', done: true)
-Task.create!(name: 'Meditate')
-Task.create!(name: 'Build first project')
+yoga = Task.create! name: 'Do yoga', done: true
+meditation = Task.create! name: 'Meditate'
+project = Task.create! name: 'Build first project'
 
 puts 'Create 1 initial session...'
-Session.create!
+Session.create! task: yoga
+Session.create! task: meditation
+Session.create! task: project
 
 puts 'Finished'
