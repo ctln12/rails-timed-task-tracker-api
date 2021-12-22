24831f3bd19959fbef15a699e801a8fa945a7990
Cataline Baert <snuf_kin@yahoo.com>
2021-12-22 16:15:49 +0100

    Clean up seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@ Task.destroy_all
 Session.destroy_all
 
 puts 'Create 3 initial tasks with 1 default session each...'
-yoga = Task.create! name: 'Do yoga', done: true
-meditation = Task.create! name: 'Meditate'
-project = Task.create! name: 'Build first project'
+Task.create! name: 'Do yoga', done: true
+Task.create! name: 'Meditate'
+Task.create! name: 'Build first project'
 
 puts 'Finished'
