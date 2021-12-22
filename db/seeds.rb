35782f593c8e5f436be13ce0df3763c28593dadb
Cataline Baert <snuf_kin@yahoo.com>
2021-12-22 16:15:05 +0100

    Add callback to Task model that creates one default session on each task creation and modify seeds consequently

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,9 @@ puts 'Cleaning database...'
 Task.destroy_all
 Session.destroy_all
 
-puts 'Create 3 initial tasks...'
+puts 'Create 3 initial tasks with 1 default session each...'
 yoga = Task.create! name: 'Do yoga', done: true
 meditation = Task.create! name: 'Meditate'
 project = Task.create! name: 'Build first project'
-
-puts 'Create 1 initial session...'
-Session.create! task: yoga
-Session.create! task: meditation
-Session.create! task: project
 
 puts 'Finished'
